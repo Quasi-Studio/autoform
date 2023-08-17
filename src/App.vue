d003f8d05c62839f6f60ca99946f5e300501645f
Zecyel <zecyel@163.com>
2023-08-17 20:59:03 +0800

    Upd: 初始化项目

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+// import { inject } from './vf'
+
+export default defineComponent({
+  name: 'App'
+});
+
+// inject('apppppp')
+
+</script>
+
+<style scoped>
+
+
+</style>
